c4de08c8a6d5a2d80b9ec93cdee83728c9fb2f91
Jeremy Greer <jex.grizzle@gmail.com>
2018-11-26 21:42:40 -0500

    Print out result after clicking internal/external url

--- README.md
+++ README.md
@@ -16,3 +16,15 @@ If you just want to see the errors without a real build...
 ```
 elm make src/Main.elm --output=/dev/null
 ```
+
+Change from document to application.
+
+Url is needed, but not installed.
+
+Once we start using application, we need to use a real server.
+
+## Serve
+
+```
+python -m SimpleHTTPServer
+```
--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,48 @@
 module Main exposing (main)
 
 import Browser
+import Browser.Navigation
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Url
 
 
 type alias Model =
-    { num : Int }
+    { num : Int
+    , message : String
+    }
 
 
 type Msg
-    = Hello
+    = UrlChanged Url.Url
+    | LinkClicked Browser.UrlRequest
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( { num = 99 }, Cmd.none )
+init : () -> Url.Url -> Browser.Navigation.Key -> ( Model, Cmd Msg )
+init flags key url =
+    ( { num = 99
+      , message = ""
+      }
+    , Cmd.none
+    )
+
+
+onUrlChange : Url.Url -> Msg
+onUrlChange url =
+    UrlChanged url
+
+
+
+{-
+   type UrlRequest
+     = Internal Url.Url
+     | External String
+-}
+
+
+onUrlRequest : Browser.UrlRequest -> Msg
+onUrlRequest req =
+    LinkClicked req
 
 
 subscriptions : Model -> Sub Msg
@@ -24,22 +52,52 @@ subscriptions model =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        LinkClicked req ->
+            case req of
+                Browser.Internal url ->
+                    ( { model
+                        | message = "internal"
+                      }
+                    , Cmd.none
+                    )
+
+                Browser.External url ->
+                    ( { model
+                        | message = url
+                      }
+                    , Cmd.none
+                    )
+
+        UrlChanged url ->
+            ( model, Cmd.none )
+
+
+viewLink : String -> Html Msg
+viewLink path =
+    div []
+        [ a [ href path ] [ text path ]
+        ]
 
 
 view : Model -> Browser.Document Msg
 view model =
     { title = "Hello!"
     , body =
-        [ div [] [ text "Hello, everyone." ]
+        [ div [] [ text model.message ]
         , div [] [ text (String.fromInt model.num) ]
+        , viewLink "/home"
+        , viewLink "/home/gingo"
+        , viewLink "https://google.com"
         ]
     }
 
 
 main =
-    Browser.document
+    Browser.application
         { init = init
+        , onUrlChange = onUrlChange
+        , onUrlRequest = onUrlRequest
         , subscriptions = subscriptions
         , update = update
         , view = view
